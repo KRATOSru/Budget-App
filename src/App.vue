72066607491b962f473fd130c25a6db08bd0bc97
vladimirKartyshov <vovan198080@inbox.ru>
2020-08-21 20:12:55 +0300

    BudgetList.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,34 @@
 <template>
   <div id="app">
-
+    <BudgetList :list="list"/>
   </div>
 </template>
 
 <script>
+  import BudgetList from "@/components/BudgetList";
 
 export default {
   name: 'App',
   components: {
-
+    BudgetList
   },
-
-
-}
+  data: () => ({
+    list: {
+      1: {
+        type: 'INCOME',
+        value: 100,
+        comment: 'Some comment',
+        id: 1
+      },
+      2: {
+        type: 'OUTCOME',
+        value: -50,
+        comment: 'Some outcome comment',
+        id: 2
+      }
+    }
+  })
+};
 </script>
 
 <style>
