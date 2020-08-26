bfc9ce42b5e04a238332c5381229ca8898abf320
vladimirKartyshov <vovan198080@inbox.ru>
2020-08-26 23:18:31 +0300

    RROJECT HAS BEEN FINISHED

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# budgetapp
+# budget-app
 
 ## Project setup
 ```
--- src/plugins/elements/index.js
+++ src/plugins/elements/index.js
@@ -1,4 +1,4 @@
-import Vue from "vue";
+import Vue from 'vue';
 import lang from 'element-ui/lib/locale/lang/en';
 import locale from 'element-ui/lib/locale';
 import 'element-ui/packages/theme-chalk/lib/index.css';
@@ -26,4 +26,6 @@ const elements = [
 
 locale.use(lang);
 
-elements.forEach(E1 => Vue.use(E1, { locale }));
+elements.forEach(El => Vue.use(El, { locale }));
+
+
